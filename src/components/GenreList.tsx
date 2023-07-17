3075ab9b26f5c862d4ac9320d8debcf95d6afcd3
chafaa <chafaaouchaou@gmail.com>
2023-07-17 09:08:32 +0100

    Show a spinner while fitching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,26 +1,29 @@
 import useGenre from "../hooks/useGenre";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { datas, error, isLoading } = useGenre();
-
+  if (error) return null;
   return (
-    <List>
-      {datas.map((data) => (
-        <ListItem key={data.id} paddingY={"10px"}>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(data.image_background)}
-            />
-            <Text fontSize="lg">{data.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <Spinner />}
+
+      <List>
+        {datas.map((data) => (
+          <ListItem key={data.id} paddingY={"10px"}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(data.image_background)}
+              />
+              <Text fontSize="lg">{data.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
-
 export default GenreList;
