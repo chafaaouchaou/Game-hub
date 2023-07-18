e35e5102c34056f31b22974ed03f9f24a3ba8b30
chafaa <chafaaouchaou@gmail.com>
2023-07-18 10:48:41 +0100

    Build searche component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,12 @@
 import logo from "../../assets/logo.webp";
 import style from "./NavBar.module.css";
 import ColorModeSwitch from "../../ColorModeSwitch";
+import SearchInput from "../SearchInput";
 const NavBar = () => {
   return (
     <nav className={style.navbar}>
       <img className={style.logo} src={logo} alt="chafaa's logo" />
+      <SearchInput />
       <ColorModeSwitch />
     </nav>
   );
